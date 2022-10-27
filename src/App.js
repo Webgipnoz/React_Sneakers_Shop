bc0d42fac3f2a59dda58e208737f45a51c217974
Slav Lomachynkyi <slavchek.3777@gmail.com>
2022-10-27 20:37:53 +0300

    start work with html Home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.scss'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className='headerLeft'>
+          <svg/>
+          <div className='headerInfo'>
+            <h3>React Sneakers</h3>
+            <p>Shop with the best sneakers!</p>
+          </div>
+        </div>
+        <ul className='headerRight'>
+          <li>
+            <svg/>
+            <span>120 Euro</span>
+          </li>
+          <li>
+            <svg/>
+          </li>
+        </ul>
       </header>
+      <div className='content'>
+        <h1>All Sneakers!</h1>
+        ........
+      </div>
     </div>
   );
 }
